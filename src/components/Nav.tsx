06f14c4af2a29048ac025f2a9b105947e456dd17
hector3211 <hectororopesa5@gmail.com>
2022-10-29 13:23:07 -0400

    re-worked main header and added menus

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
 import React,{useState} from "react"
 import { IoApps } from "react-icons/io5";
+import {GiDonut} from "react-icons/gi"
 
 export default function Navbar(){
     const [nav,setNav] = useState(false)
@@ -10,18 +11,23 @@ export default function Navbar(){
             setNav(true)
         }
     }
-        return (
-            <div className="flex justify-between items-center mx-auto relative px-2 py-2">
-                <a className="text-2xl font-bold hover:animate-bounce" href="/">Invy donuts</a>
-                <div className="flex justify-center items-center ">
-                <button className="text-3xl lg:text-4xl" onClick={handleToggleNav}>{<IoApps/>}</button>
-                </div>
-                {nav && (
-                <div className="z-50 flex flex-col justify-between items-center p-2 absolute right-10 top-10 rounded-md bg-zinc-800 w-24">
-                    <a className="py-2" href="/">Home</a>
-                    <a className="py-2" href="/about">About</a>
-                </div>
-                )}
+    return (
+        <div className="z-50 fixed top-0 backdrop-blur-md w-full flex justify-between items-center px-5 py-1 mx-auto">
+            <div className="flex items-center text-3xl lg:text-4xl">
+            <a className="font-bold" href="/">Invy</a>
+            <span className="ml-2"><GiDonut/></span>
+            <p>'s</p>
             </div>
+            <div className="flex justify-center items-center relative">
+                <button className="text-3xl lg:text-4xl" onClick={handleToggleNav}>{<IoApps/>}</button>
+            </div>
+            {nav && (
+                <div className="z-50 backdrop-blur-md bg-zinc-500/75 flex flex-col justify-between items-center p-2 absolute right-10 top-14 rounded-md w-24">
+                    <a className="py-1" href="/">Home</a>
+                    <a className="py-1" href="#menu">Menu</a>
+                    <a className="py-1" href="/about">About</a>
+                </div>
+            )}
+        </div>
         )
 }
