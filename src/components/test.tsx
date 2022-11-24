2e7c97e93d3714f486fdc843f805307c9bbb1a56
hector3211 <hectororopesa5@gmail.com>
2022-11-24 11:55:25 -0500

    git test on wsl

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,7 +1,7 @@
-import React from "react"
-
+import   React from "react"
 export default function Test(){
         return (
         <div>Hello</div>
+        <div> tesst git on wsl </div>
         )
     }
